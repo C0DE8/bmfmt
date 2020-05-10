90fcaeaefd86a514479e3e90fbab530772034dae
beitsolutions <bitbucket@be-it-solutions.com>
2020-05-10 10:16:51 +0200

    clean up module code + fix sometimes failing test

--- bmfmt_test.go
+++ bmfmt_test.go
@@ -1,51 +1,52 @@
 package bmfmt
 
 import (
-	"errors"
-	"github.com/c0de8/bmfmt"
 	"github.com/kami-zh/go-capturer"
 	"github.com/stretchr/testify/assert"
 	"testing"
 )
 
-var notAMapError = errors.New("given argument is NOT a map")
-
 // TestBeautifyCauseAnErrorOnNonMap test the "Beautify" function to return an error on given argument is not a map
 func TestBeautifyCauseAnErrorOnNonMap(t *testing.T) {
 
 	candidate := "not a map, just a string"
-	expectedError := notAMapError
 
-	err := bmfmt.Beautify(candidate)
-	assert.Equal(t, expectedError, err)
+	err := Beautify(candidate)
+
+	if !assert.Error(t, err, "error is expected") {
+		t.FailNow()
+	}
+	assert.Equal(t, "given argument is NOT a map", err.Error())
 }
 
 // TestBeautifyOutputASimpleMap test a valid map[string][]string output
 func TestBeautifyOutputASimpleMap(t *testing.T) {
 
-	candidate := map[string][]string{
-		"a key":    {"a single value"},
+	var err error
+	candidateSingleValue := map[string][]string{
+		"a key": {"a single value"},
+	}
+
+	// due to the fact, that maps are NOT sorted, the output order may vary, so we ned to test both orderings
+	expectedOutputSingleValue := `[ "a key"  string(  5) ]: "a single value"  string( 14)
+`
+	output := capturer.CaptureStdout(func() {
+		err = Beautify(candidateSingleValue)
+	})
+
+	assert.NoError(t, err, "nil is expected")
+	assert.Equal(t, expectedOutputSingleValue, output, "beautified map output expected (1)")
+
+	candidateMultipleValue := map[string][]string{
 		"next key": {"value one", "value two"},
 	}
 
-	// due to the fact, that maps NOT sorted, the output order may vary
-	expectedOutput1 := `[ "a key"     string(  5) ]: "a single value"          string( 14)
-[ "next key"  string(  8) ]: "value one", "value two"  string( 22)
+	expectedOutputMultipleValue := `[ "next key"  string(  8) ]: "value one", "value two"  string( 22)
 `
-	expectedOutput2 := `[ "next key"  string(  8) ]: "value one", "value two"  string( 22)
-[ "a key"     string(  5) ]: "a single value"          string( 14)
-`
-	var err error
-
-	output := capturer.CaptureStdout(func() {
-		err = bmfmt.Beautify(candidate)
+	output = capturer.CaptureStdout(func() {
+		err = Beautify(candidateMultipleValue)
 	})
 
-	assert.Nil(t, err)
-
-	firstTry := assert.Equal(t, expectedOutput1, output)
-
-	if firstTry != true {
-		assert.Equal(t, expectedOutput2, output)
-	}
+	assert.NoError(t, err, "nil is expected")
+	assert.Equal(t, expectedOutputMultipleValue, output, "beautified map output expected (2)")
 }
--- go.mod
+++ go.mod
@@ -5,5 +5,4 @@ go 1.14
 require (
 	github.com/kami-zh/go-capturer v0.0.0-20171211120116-e492ea43421d
 	github.com/stretchr/testify v1.3.0
-	gopkg.in/c0de8/bmfmt.v0 v0.0.2 // indirect
 )
